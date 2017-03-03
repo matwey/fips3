57e2c78613a5820551a79c1bb6fbc58544a1cb9c
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2017-03-03 20:45:58 +0300

    Add initial implemenation for command line arguments parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
-#include <QtGui/QApplication>
+#include <QApplication>
+#include <QCommandLineParser>
+
 #include <mainwindow.h>
 
 int main(int argc, char** argv) {
 	QApplication app(argc, argv);
+
+	QCoreApplication::setApplicationName("fips");
+	QCoreApplication::setApplicationVersion("3.0");
+
+	QCommandLineParser parser;
+	parser.addHelpOption();
+	parser.addPositionalArgument("file", QCoreApplication::translate("main", "The file to open."));
+
+	parser.process(app);
+
+	const QStringList args = parser.positionalArguments();
 
 	MainWindow w;
 	w.show();
