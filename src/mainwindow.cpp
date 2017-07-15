177c29d65da25cb7215c7b8037123825c7c0103b
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2017-07-15 23:47:16 +0300

    Add FIXME: notification into MainWindow::MainWindow

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(const QString& fits_filename):
 	QMainWindow() {
 
 	QFile* file = new QFile(fits_filename);
+	// FIXME: check open return value
+	// http://doc.qt.io/qt-5/qfile.html#open-1
 	file->open(QIODevice::ReadOnly);
 	open_gl_widget_.reset(new OpenGLWidget(this, new FITS(file)));
 
