7d751c9dd0625fc68f7da83af0bdc1303cc56ee8
Konstantin Malanchev <hombit@gmail.com>
2017-07-25 16:12:41 +0300

    Zoom via menu actions
    Add zoomIn and zoomOut menu actions that performs zooming of OpenGL
    widget.

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -2,7 +2,9 @@
 #define _MAINWINDOW_H_
 
 #include <QMainWindow>
+#include <QMenuBar>
 #include <QScrollArea>
+#include <QScrollBar>
 
 #include <memory>
 
@@ -26,10 +28,24 @@ public:
 		virtual QException* clone() const override;
 	};
 private:
+	std::unique_ptr<QMenuBar> menu_bar_;
+	std::unique_ptr<QMenu> view_menu_;
+	std::unique_ptr<QAction> zoomIn_action_;
+	std::unique_ptr<QAction> zoomOut_action_;
+
 	std::unique_ptr<QScrollArea>  scroll_area_;
 	std::unique_ptr<OpenGLWidget> open_gl_widget_;
+
+	double scale_factor_ = 1.0;
+	std::unique_ptr<QSize> fits_size_;
+
+	void zoomIn();
+	void zoomOut();
+	void zoom(double zoom_factor);
+	void adjustScrollBar(QScrollBar *scrollBar, double zoom_factor);
 public:
 	MainWindow(const QString& fits_filename);
+	double scale_factor();
 };
 
 #endif // _MAINWINDOW_H_
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -21,18 +21,64 @@ QException* MainWindow::FileOpenError::clone() const {
 }
 
 MainWindow::MainWindow(const QString& fits_filename): QMainWindow() {
-
+	// Open FITS file
 	std::unique_ptr<QFile> file{new QFile(fits_filename)};
 	if (!file->open(QIODevice::ReadOnly)) {
 		throw FileOpenError(file->errorString());
 	}
 
+	// Read FITS from file
 	std::unique_ptr<FITS> fits{new FITS(file.release())};
-	resize(fits->data_unit().width(), fits->data_unit().height());
+	fits_size_.reset(new QSize(fits->data_unit().width(), fits->data_unit().height()));
 
+	// Resize window to fit FITS image
+	// FIXME: reduce window size if FITS larger than Desktop and change scale_factor_
+	resize(*fits_size_);
+
+	// Create scroll area and put there open_gl_widget
 	scroll_area_.reset(new QScrollArea(this));
-	scroll_area_->resize(size());
+	setCentralWidget(scroll_area_.get());
 	open_gl_widget_.reset(new OpenGLWidget(scroll_area_.get(), fits.release()));
 	open_gl_widget_->resize(size());
 	scroll_area_->setWidget(open_gl_widget_.get());
+
+	menu_bar_.reset(new QMenuBar(nullptr));
+	view_menu_.reset(menu_bar_->addMenu(tr("&View")));
+
+	zoomIn_action_.reset (view_menu_->addAction(tr("Zoom &In (25%)"),  this, &MainWindow::zoomIn));
+	zoomIn_action_->setShortcut (QKeySequence::ZoomIn);
+
+	zoomOut_action_.reset(view_menu_->addAction(tr("Zoom &Out (25%)"), this, &MainWindow::zoomOut));
+	zoomOut_action_->setShortcut(QKeySequence::ZoomOut);
+}
+
+void MainWindow::zoomIn() {
+	zoom(1.25);
+}
+
+void MainWindow::zoomOut() {
+	zoom(0.80);
+}
+
+void MainWindow::zoom(double zoom_factor) {
+	scale_factor_ *= zoom_factor;
+
+	// Resize OpenGL widget
+	auto widget_size = QSize(*fits_size_);
+	widget_size.scale(widget_size * scale_factor_, Qt::KeepAspectRatio);
+	open_gl_widget_->resize(widget_size);
+
+	// Keep scrolls on the same relative position
+	adjustScrollBar(scroll_area_->horizontalScrollBar(), zoom_factor);
+	adjustScrollBar(scroll_area_->verticalScrollBar(),   zoom_factor);
+}
+
+void MainWindow::adjustScrollBar(QScrollBar* scroll_bar, double zoom_factor) {
+	scroll_bar->setValue(
+			static_cast<int>(zoom_factor * scroll_bar->value() + ((zoom_factor - 1) * scroll_bar->pageStep()/2))
+	);
+}
+
+double MainWindow::scale_factor() {
+	return scale_factor_;
 }
