9b7cf6f4ce7e024fa6f1b07ddbdb5c99f219255f
Konstantin Malanchev <hombit@gmail.com>
2017-08-03 17:33:22 +0300

    Connect sliders and spinboxes to each other
    Introduce new class DoubleSlider that is variant of QSlider for real
    values.
    Connect pairs of sliders and spinboxes to each other via slots and
    signals

--- include/levelswidget.h
+++ include/levelswidget.h
@@ -4,10 +4,51 @@
 #include <QDebug>
 #include <QDoubleSpinBox>
 #include <QGridLayout>
+#include <QResizeEvent>
 #include <QSlider>
+#include <QStyle>
 #include <QWidget>
 
 #include <memory>
+
+class DoubleSlider: public QSlider {
+	Q_OBJECT
+
+private:
+	double double_minimum_, double_maximum_;
+	double double_value_;
+
+	// Protect public methods from parent class
+	int minimum() const;
+	int maximum() const;
+	int pageStep() const;
+	int singleStep() const;
+	void scroll(int, int);
+	void scroll(int, int, const QRect&);
+	void setMinimum(int);
+	void setMaximum(int);
+	void setPageStep(int);
+	void setRange(int, int);
+	void setSingleStep(int);
+	void setSliderPosition(int);
+	void setValue(int);
+	int value() const;
+
+protected:
+	void resizeEvent(QResizeEvent* event) override;
+
+public:
+	DoubleSlider(Qt::Orientation orientation, QWidget* parent=Q_NULLPTR, double minimum=0, double maximum=1);
+
+signals:
+	void valueChanged(double value);
+
+public slots:
+	void setValue(double value);
+
+private slots:
+	void notifyValueChanged(int value);
+};
 
 class LevelsWidget: public QWidget {
 public:
--- src/levelswidget.cpp
+++ src/levelswidget.cpp
@@ -1,11 +1,58 @@
 #include <levelswidget.h>
 
-LevelsWidget::LevelsWidget(QWidget* parent): QWidget(parent) {
-	std::unique_ptr<QSlider> min_slider{new QSlider(Qt::Horizontal, this)};
-	std::unique_ptr<QSlider> max_slider{new QSlider(Qt::Horizontal, this)};
+DoubleSlider::DoubleSlider(Qt::Orientation orientation, QWidget *parent, double minimum, double maximum):
+		QSlider(orientation, parent),
+		double_minimum_(minimum),
+		double_maximum_(maximum),
+		double_value_(double_minimum_) {
+	connect(this, SIGNAL(valueChanged(int)), this, SLOT(notifyValueChanged(int)));
+}
 
+void DoubleSlider::resizeEvent(QResizeEvent *event) {
+	int length;
+	if (orientation() == Qt::Horizontal) {
+		length = event->size().width();
+	} else {
+		length = event->size().height();
+	}
+	QSlider::setRange(0, length-1);
+	setValue(double_value_);
+}
+
+void DoubleSlider::setValue(double value) {
+	if (value < double_minimum_) {
+		value = double_minimum_;
+	} else if (value > double_maximum_) {
+		value = double_maximum_;
+	}
+
+	const int new_int_value = qRound((double_value_ - double_minimum_) / (double_maximum_ - double_minimum_)
+			* (QSlider::maximum() - QSlider::minimum()));
+	QSlider::setValue(new_int_value);
+
+	if (double_value_ != value) {
+		double_value_ = value;
+		emit valueChanged(double_value_);
+	}
+}
+
+void DoubleSlider::notifyValueChanged(int value) {
+	const auto new_double_value = static_cast<double>(value - QSlider::minimum()) / (QSlider::maximum() - QSlider::minimum())
+				* (double_maximum_ - double_minimum_)
+				+ double_minimum_;
+	setValue(new_double_value);
+}
+
+LevelsWidget::LevelsWidget(QWidget* parent): QWidget(parent) {
+	std::unique_ptr<DoubleSlider> min_slider{new DoubleSlider(Qt::Horizontal, this)};
 	std::unique_ptr<QDoubleSpinBox> min_spinbox{new QDoubleSpinBox(this)};
+	connect(min_spinbox.get(), SIGNAL(valueChanged(double)), min_slider.get(), SLOT(setValue(double)));
+	connect(min_slider.get(), SIGNAL(valueChanged(double)), min_spinbox.get(), SLOT(setValue(double)));
+
+	std::unique_ptr<DoubleSlider> max_slider{new DoubleSlider(Qt::Horizontal, this)};
 	std::unique_ptr<QDoubleSpinBox> max_spinbox{new QDoubleSpinBox(this)};
+	connect(max_spinbox.get(), SIGNAL(valueChanged(double)), max_slider.get(), SLOT(setValue(double)));
+	connect(max_slider.get(), SIGNAL(valueChanged(double)), max_spinbox.get(), SLOT(setValue(double)));
 
 	std::unique_ptr<QGridLayout> grid_layout{new QGridLayout(this)};
 	grid_layout->addWidget(min_slider .release(), 0, 0);
