fc4625d227fea3fc54c17fb94c67d8b7829191aa
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2018-03-20 11:22:12 +0300

    Add initial implementation for AbstractOpenGLPlan

---
+++ include/abstractopenglplan.h
@@ -0,0 +1,31 @@
+/*
+ *  Copyright (C) 2018  Matwey V. Kornilov <matwey.kornilov@gmail.com>
+ *                      Konstantin Malanchev <hombit@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or (at
+ *  your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _ABSTRACTOPENGLPLAN_H
+#define _ABSTRACTOPENGLPLAN_H
+
+#include <QObject>
+
+class AbstractOpenGLPlan:
+	public QObject {
+public:
+	AbstractOpenGLPlan(QObject* parent = Q_NULLPTR);
+	virtual ~AbstractOpenGLPlan() = 0;
+};
+
+#endif // _ABSTRACTOPENGLPLAN_H
