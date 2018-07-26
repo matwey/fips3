matwey/fips3

commit 04655711959b69c08fcecf64b07f971549738716

Add initial implementation for OpenGLFeatures and OpenGLRequiredFeatures

src/openglfeatures.cpp
@@ -1,0 +1,49 @@
+/*
+ *  Copyright (C) 2018  Matwey V. Kornilov <matwey.kornilov@gmail.com>
+ *                      Konstantin Malanchev <hombit@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or (at
+ *  your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <openglfeatures.h>
+
+constexpr const quint64 OpenGLFeatures::mask_hasOpenGL21;
+constexpr const quint64 OpenGLFeatures::mask_hasOpenGL30;
+constexpr const quint64 OpenGLFeatures::mask_hasOpenGL33;
+
+bool OpenGLFeatures::checkHasOpenGLHelper(const QOpenGLContext& opengl_context, const int major, const int minor, const quint64 mask) {
+	bool has = (opengl_context.format().majorVersion() > major
+		|| (opengl_context.format().majorVersion() == major && opengl_context.format().minorVersion() >= minor));
+	bitmask_ |= (has ? mask : static_cast<quint64>(0));
+	return has;
+}
+
+bool OpenGLFeatures::checkHasOpenGL33(const QOpenGLContext& opengl_context) {
+	return checkHasOpenGLHelper(opengl_context, 3, 3,
+		mask_hasOpenGL33 | mask_hasOpenGL30 | mask_hasOpenGL21);
+}
+
+bool OpenGLFeatures::checkHasOpenGL30(const QOpenGLContext& opengl_context) {
+	return checkHasOpenGLHelper(opengl_context, 3, 0, mask_hasOpenGL30 | mask_hasOpenGL21);
+}
+
+bool OpenGLFeatures::checkHasOpenGL21(const QOpenGLContext& opengl_context) {
+	return checkHasOpenGLHelper(opengl_context, 2, 1, mask_hasOpenGL21);
+}
+
+OpenGLFeatures::OpenGLFeatures(const QOpenGLContext& opengl_context) {
+	checkHasOpenGL33(opengl_context)
+		|| checkHasOpenGL30(opengl_context)
+		|| checkHasOpenGL21(opengl_context);
+}

include/openglfeatures.h
@@ -1,0 +1,84 @@
+/*
+ *  Copyright (C) 2018  Matwey V. Kornilov <matwey.kornilov@gmail.com>
+ *                      Konstantin Malanchev <hombit@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or (at
+ *  your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _OPENGLFEATURES_H
+#define _OPENGLFEATURES_H
+
+#include <QtGlobal>
+#include <QOpenGLContext>
+
+class OpenGLFeatures {
+protected:
+	quint64 bitmask_;
+
+	constexpr static const quint64 mask_hasOpenGL21 = Q_UINT64_C(1) << 1;
+	constexpr static const quint64 mask_hasOpenGL30 = Q_UINT64_C(1) << 2;
+	constexpr static const quint64 mask_hasOpenGL33 = Q_UINT64_C(1) << 3;
+private:
+	bool checkHasOpenGLHelper(const QOpenGLContext& opengl_context, const int major, const int minor, const quint64 mask);
+
+	bool checkHasOpenGL33(const QOpenGLContext& opengl_context);
+	bool checkHasOpenGL30(const QOpenGLContext& opengl_context);
+	bool checkHasOpenGL21(const QOpenGLContext& opengl_context);
+protected:
+	constexpr inline OpenGLFeatures(const quint64 bitmask = 0): bitmask_(bitmask) {}
+public:
+	explicit OpenGLFeatures(const QOpenGLContext& opengl_context);
+
+	OpenGLFeatures(const OpenGLFeatures&) = default;
+	OpenGLFeatures(OpenGLFeatures&&) = default;
+	OpenGLFeatures& operator=(const OpenGLFeatures&) = default;
+	OpenGLFeatures& operator=(OpenGLFeatures&&) = default;
+
+	constexpr inline bool operator==(const OpenGLFeatures& other) const {
+		return bitmask_ == other.bitmask_;
+	}
+
+	constexpr inline OpenGLFeatures operator& (const OpenGLFeatures& other) const {
+		return OpenGLFeatures{bitmask_ & other.bitmask_};
+	}
+};
+
+class OpenGLRequiredFeatures:
+	public OpenGLFeatures {
+private:
+	constexpr inline OpenGLRequiredFeatures(const quint64 bitmask): OpenGLFeatures(bitmask) {}
+public:
+	constexpr inline OpenGLRequiredFeatures(): OpenGLFeatures() {}
+
+	OpenGLRequiredFeatures(const OpenGLRequiredFeatures&) = default;
+	OpenGLRequiredFeatures(OpenGLRequiredFeatures&&) = default;
+	OpenGLRequiredFeatures& operator=(const OpenGLRequiredFeatures&) = default;
+	OpenGLRequiredFeatures& operator=(OpenGLRequiredFeatures&&) = default;
+
+	constexpr inline bool satisfied(const OpenGLFeatures& features) const {
+		return (features & *this) == *this;
+	}
+
+	constexpr inline OpenGLRequiredFeatures requireOpenGL21() const {
+		return OpenGLRequiredFeatures{bitmask_ | mask_hasOpenGL21};
+	}
+	constexpr inline OpenGLRequiredFeatures requireOpenGL30() const {
+		return OpenGLRequiredFeatures{bitmask_ | mask_hasOpenGL30};
+	}
+	constexpr inline OpenGLRequiredFeatures requireOpenGL33() const {
+		return OpenGLRequiredFeatures{bitmask_ | mask_hasOpenGL33};
+	}
+};
+
+#endif // _OPENGLFEATURES_H

commit 87e819f7e24ae60610c3bc9379b605ee323bdf6f

Do not set OpenGL 3.0 and OpenGL 2.1 flags for OpenGL 3.3
Fixes: 04655711959b ("Add initial implementation for OpenGLFeatures and OpenGLRequiredFeatures")

src/openglfeatures.cpp
@@ -30,8 +30,7 @@ bool OpenGLFeatures::checkHasOpenGLHelper(const QOpenGLContext& opengl_context, 
 }
 
 bool OpenGLFeatures::checkHasOpenGL33(const QOpenGLContext& opengl_context) {
-	return checkHasOpenGLHelper(opengl_context, 3, 3,
-		mask_hasOpenGL33 | mask_hasOpenGL30 | mask_hasOpenGL21);
+	return checkHasOpenGLHelper(opengl_context, 3, 3, mask_hasOpenGL33);
 }
 
 bool OpenGLFeatures::checkHasOpenGL30(const QOpenGLContext& opengl_context) {